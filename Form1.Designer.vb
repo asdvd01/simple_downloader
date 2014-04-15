3fd78c18c6340702ee1de53146db1cb9609e7183
Ashutosh Dwivedi <ashutoshdvd01@gmail.com>
2014-04-15 19:19:32 +0530

    a simple good looking downloader in visual basic

---
+++ Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(22, 21)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(602, 31)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Enter url here"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(22, 67)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(602, 31)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "Enter directory"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Britannic Bold", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(22, 113)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(339, 31)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = "Enter filename"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.Brown
+        Me.ProgressBar1.Location = New System.Drawing.Point(22, 159)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(339, 41)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(415, 112)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(209, 31)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "download"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Location = New System.Drawing.Point(418, 159)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(209, 41)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "close"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 23.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(642, 230)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Britannic Bold", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "Form1"
+        Me.Text = "downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Net
+Public Class Form1
+
+    Public WithEvents download As New WebClient
+
+  
+
+    Private Sub TextBox2_Clicked(sender As Object, e As EventArgs) Handles TextBox2.Click
+        TextBox2.Text = ""
+        FolderBrowserDialog1.ShowDialog()
+        TextBox2.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As String
+        a = TextBox1.Text.ToString()
+        If (Not a.StartsWith("http://")) Then
+            TextBox1.Text = "http://" & TextBox1.Text
+        End If
+
+        download.DownloadFileAsync(New Uri(TextBox1.Text), TextBox2.Text + "\" + TextBox3.Text)
+
+    End Sub
+
+    Private Sub download_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs) Handles download.DownloadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub download_DownloadFileCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs) Handles download.DownloadFileCompleted
+
+        MessageBox.Show("download complete")
+
+    End Sub
+
+ 
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub TextBox3_Click(sender As Object, e As EventArgs) Handles TextBox3.Click
+        TextBox3.Text = ""
+    End Sub
+
+    
+    
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+End Class
